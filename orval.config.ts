f78d067370acf015e309d8c7b474d43abb01acc7
neko-no <101441574+neko-no@users.noreply.github.com>
2025-08-11 15:25:29 +0900

    feat: Orvalからコードを生成 #7

---
+++ orval.config.ts
@@ -0,0 +1,16 @@
+import { defineConfig } from "orval";
+
+/**
+ * Orval Configuration
+ * @see https://orval.dev/reference/configuration/overview
+ */
+export default defineConfig({
+  notice: {
+    input: {
+      target: "./schema/tsp-output/schema/openapi.yaml",
+    },
+    output: {
+      target: "./frontend/src/schema",
+    },
+  },
+});
