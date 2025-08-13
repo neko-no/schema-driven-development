03eba313d4c09927474ef02ee4b983d1fae298fa
neko-no <101441574+neko-no@users.noreply.github.com>
2025-08-13 14:24:19 +0900

    faet: TanstackQueryにてクエリパラメータを変更した際の挙動を定義 #13

--- frontend/src/app/tanstack-demo/page.tsx
+++ frontend/src/app/tanstack-demo/page.tsx
@@ -2,22 +2,33 @@
 
 import { useQuery } from "@tanstack/react-query";
 import { resolve } from "path";
+import { useState } from "react";
 
 export default function TanstackDemo() {
-  const { data, isPending, refetch } = useQuery({
-    queryKey: ["todos"],
-    queryFn: getTodos,
+  const [id, setId] = useState(1);
+  const { data, isPending } = useQuery({
+    queryKey: ["todos", id],
+    queryFn: () => getComments(id),
   });
+
   return (
     <>
-      <div>{isPending ? "Pending" : JSON.stringify(data.slice(0, 10))}</div>
-      <button onClick={() => refetch()}>Refetch</button>
+      <div>{isPending ? "Pending" : JSON.stringify(data?.slice(0, 10))}</div>
+      <button
+        onClick={() => {
+          setId((prev) => prev + 1);
+        }}
+      >
+        Increment ID
+      </button>
     </>
   );
 }
 
-const getTodos = async () => {
+const getComments = async (id: number) => {
   await new Promise((resolve) => setTimeout(resolve, 1000));
-  const response = fetch("https://jsonplaceholder.typicode.com/todos");
+  const response = fetch(
+    `https://jsonplaceholder.typicode.com/comments?postId=${id}`
+  );
   return (await response).json();
 };
