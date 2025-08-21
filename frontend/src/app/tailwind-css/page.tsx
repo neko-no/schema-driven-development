8fe89f6e55b1f113fb08a9cf29ea9bc32085869b
neko-no <101441574+neko-no@users.noreply.github.com>
2025-08-21 20:29:07 +0900

    feat: shadcn/uiの導入 #19

--- frontend/src/app/tailwind-css/page.tsx
+++ frontend/src/app/tailwind-css/page.tsx
@@ -1,3 +1,12 @@
+import { Button } from "@/components/ui/button";
+
 export default function Home() {
-  return <h1 className="text-3xl font-bold underline">Hello world!</h1>;
+  return (
+    <div className="flex flex-wrap items-center gap-2 md:flex-row">
+      <Button size="sm" variant="link">
+        Click me
+      </Button>
+      <h1 className="text-3xl font-bold underline">Hello world!</h1>
+    </div>
+  );
 }
