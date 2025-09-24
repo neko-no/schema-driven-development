7b09473384fc5ad8ec5b3f3af22e5e4fba04bc5b
neko-no <101441574+neko-no@users.noreply.github.com>
2025-09-24 22:58:42 +0900

    faet: Vitest実行時の設定の変更 #30

--- frontend/vitest.config.ts
+++ frontend/vitest.config.ts
@@ -1,6 +1,9 @@
-import { defineConfig } from 'vitest/config'
+import { defineConfig } from 'vitest/config';
+import tsconfigPaths from 'vite-tsconfig-paths';
+import react from '@vitejs/plugin-react';
 
 export default defineConfig({
+  plugins: [react(),tsconfigPaths()],
   test: {
     name: "browser",
     include: ["src/**/*.test.tsx"],
@@ -9,6 +12,7 @@ export default defineConfig({
         name: "chromium",
         provider: "playwright",
         providerOptions: {}
-    }
+    },
+    globals: true,
   },
 })
